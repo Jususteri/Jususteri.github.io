7fceb54f8dc431aa8314aafea84c567f2cfa90ce
jereluo <jere.luomanmaki@outlook.com>
2023-05-21 14:50:11 +0300

    ud

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ $('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
         backgroundSize: 'contain',
         width: '100%',
         height: '100%',
-        loading: 'lazy', 
         position: 'fixed',
         zIndex: '10000',
         top: '0',
@@ -28,35 +27,6 @@ $('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
     });
 });
 
-// lazy loading
-let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(function (entry) {
-      if (entry.intersectionRatio > 0 || entry.isIntersecting) {
-        const image = entry.target;
-        observer.unobserve(image);
-  
-        if (image.hasAttribute('src')) {
-          // Image has been loaded already
-          return;
-        }
-  
-        // Image has not been loaded so load it
-        const sourceUrl = image.getAttribute('data-src');
-        image.setAttribute('src', sourceUrl);
-  
-        image.onload = () => {
-          // Do stuff
-        }
-  
-        // Removing the observer
-        observer.unobserve(image);
-      }
-    });
-  });
-  
-  document.querySelectorAll('.lazyload').forEach((el) => {
-    observer.observe(el);
-  });
 
 //Chat Plugin Code
 var chatbox = document.getElementById('fb-customer-chat');
